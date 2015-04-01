b15eeee2b2e667ae9aa74c60b823eaf0ebb94127
yogipriyo <yogipriyo.prayogo@gmail.com>
2015-04-01 10:31:08 +0000

    add model session 4

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,9 @@ class StaticPagesController < ApplicationController
 	before_action :age
 
 	def home
-		@name  = params['name']
+		#@name  = params['name']
+    #@places = Place.all
+    @places = Place.limit(8)
 	end
 
 	def about
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# kost_list = [
+#   { name: 'Kosan1', description: 'Kost bersih, indah, nyaman, aman dan tenteram1' },
+#   { name: 'Kosan2', description: 'Kost bersih, indah, nyaman, aman dan tenteram2' },
+#   { name: 'Kosan3', description: 'Kost bersih, indah, nyaman, aman dan tenteram3' },
+#   { name: 'Kosan4', description: 'Kost bersih, indah, nyaman, aman dan tenteram4' },
+#   { name: 'Kosan5', description: 'Kost bersih, indah, nyaman, aman dan tenteram5' },
+#   { name: 'Kosan6', description: 'Kost bersih, indah, nyaman, aman dan tenteram6' },
+#   { name: 'Kosan7', description: 'Kost bersih, indah, nyaman, aman dan tenteram7' },
+#   { name: 'Kosan8', description: 'Kost bersih, indah, nyaman, aman dan tenteram8' },
+#   { name: 'Kosan9', description: 'Kost bersih, indah, nyaman, aman dan tenteram9' },
+#   { name: 'Kosan10', description: 'Kost bersih, indah, nyaman, aman dan tenteram10' }
+# ]
+
+# kost_list.each do |k|
+#   Place.create(k)
+# end
+
+
+#lebih hemat resource karena hanya mengeksekusi query 1 kali, sedangkan yang diatas query akan
+#diulang sebanyak jumlah array
+kost_list = [
+  {name: "Last", description: "Last"},
+  {name: "Last 2", description: "Last 2"}
+]
+
+Place.create(kost_list)
