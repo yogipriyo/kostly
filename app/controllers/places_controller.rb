90da7e4d505ad86fa186b3baeed1b04530d74001
yogipriyo <yogipriyo.prayogo@gmail.com>
2015-04-07 09:16:41 +0000

    add show, edit & destroy place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,6 +14,29 @@ class PlacesController < ApplicationController
 
     redirect_to places_url, notice: 'Place created!'
   end
+  
+  def edit
+    @update_place = Place.find(params[:id])
+  end
+  
+  def update
+    #place_params itu dari mana ya?
+    @update_place = Place.find(params[:id])
+    @update_place.update(place_params)
+    redirect_to places_index_path
+  end
+  
+  def show
+    @single_place = Place.find(params[:id])
+    #render json: params
+    #render json: @single_place
+  end
+  
+  def destroy
+    place = Place.find(params[:id])
+    place.destroy
+    redirect_to places_index_path
+  end
 
   private
 
